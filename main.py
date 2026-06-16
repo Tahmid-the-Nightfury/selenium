2b79b4874b6f25cae91c24fa3fb4435ee40bb5d7
Tahmid-the-Nightfury <admfardin@gmail.com>
2026-06-17 00:14:10 +0600

    project 1 scraping all the data of  something from amazon website

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import time
 
 driver = webdriver.Chrome()
 query = "laptop"
-driver.get(f"https://www.amazon.in/s?k={query}&crid=2XES8RUBB5SBD&sprefix=laptop%2Caps%2C294&ref=nb_sb_noss_2")
-elem = driver.find_elements(By.CLASS_NAME, "puis-card-container")
-#print(elem.text)
-print(f"{len(elem)} items found")
-for elems in elem:
-    print(elems.text)
+for i in range(1,20):
+    
+    driver.get(f"https://www.amazon.in/s?k={query}&page={i}&xpid=DlNNJmH6ckBXM&crid=2XES8RUBB5SBD&qid=1781588880&sprefix=laptop%2Caps%2C294&ref=sr_pg_2")
+    elem = driver.find_elements(By.CLASS_NAME, "puis-card-container")
+    #print(elem.text)
+    print(f"{len(elem)} items found")
+    for elems in elem:
+        print(elems.text)
 time.sleep(6)
 driver.close()
